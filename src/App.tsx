b204128dd5af5b8ef2ced25bfb01f0e4518416f2
Svygzhryr <pamixy@gmail.com>
2023-08-01 09:07:31 +0300

    chore: checking husky

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+        <img src={logo} 
+        className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
