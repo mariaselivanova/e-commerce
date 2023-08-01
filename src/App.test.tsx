df4c175f123549ac581710b17c221b4eab26fe96
Svygzhryr <pamixy@gmail.com>
2023-08-01 14:48:05 +0300

    chore: jest + ts-jest initialize

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// sanity check
+it('one is one', () => {
+  expect(1).toBe(1);
 });
