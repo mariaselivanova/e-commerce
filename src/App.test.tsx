45eaebe516a7ed7eb39d754ab34bc27d8896ccd2
Svygzhryr <pamixy@gmail.com>
2023-08-01 15:02:55 +0300

    chore: jest initialize

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// sanity check
-it('one is one', () => {
+it('sanity check', () => {
   expect(1).toBe(1);
 });
+
+// test('debugs whole app', () => {
+//   render(<App />);
+//   screen.debug();
+// });
