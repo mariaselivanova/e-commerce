73ebd2a8f8b46faef0b494b13695ccb496f1de52
Svygzhryr <pamixy@gmail.com>
2023-08-01 10:47:20 +0300

    chore: husky testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,11 +4,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  
-  const linkElement = screen.getByText(
-    /learn react/i
-    );
-  expect(linkElement).toBeInTheDocument(
-    
-  );
+
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
