8f60f0d8f888a33a4ff80a2d583235050cef605c
Maria Selivanova <maria.m.selivanova@yandex.ru>
2023-08-02 20:04:34 +0300

    chore: update imports

--- src/components/app/App.test.tsx
+++ src/components/app/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './app';
 
 it('sanity check', () => {
   expect(1).toBe(1);
