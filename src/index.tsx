99dfe77fc19cf3bdc12700616bbb09f61c289847
Dana Karpova <devil13066@gmail.com>
2023-08-09 17:12:00 +0400

    feat: make theme

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,20 @@ import reportWebVitals from './reportWebVitals';
 
 import { App } from './components/App';
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import { createTheme, ThemeProvider, CssBaseline } from '@mui/material';
+import { themeOptions } from './theme/ThemeOptions';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const theme = createTheme(themeOptions);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 );
 
