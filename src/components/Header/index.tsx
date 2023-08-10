13d9e175e1c3a59004f01d3faa4d3c491179372d
Dana Karpova <devil13066@gmail.com>
2023-08-10 19:41:02 +0400

    fix: edit header to theme style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useContext } from 'react';
 import { useLocation, Link } from 'react-router-dom';
-import { Stack, Typography } from '@mui/material';
+import { Stack, Typography, Button } from '@mui/material';
 
 import { UserContext } from '../../contexts/userContext';
 
@@ -14,11 +14,17 @@ export const Header: FC = () => {
   const isMainRoute = pathname === '/';
 
   const buttons = user ? (
-    <AuthLink to={'/'}>Logout</AuthLink>
+    <Button variant='contained' href={'/'}>
+      Logout
+    </Button>
   ) : isMainRoute ? (
     <>
-      <AuthLink to={'/login'}>Log in</AuthLink>
-      <AuthLink to={'/register'}>Register</AuthLink>
+      <Button variant='contained' href={'/login'}>
+        Log in
+      </Button>
+      <Button variant='contained' href={'/register'}>
+        Register
+      </Button>
     </>
   ) : null;
 
