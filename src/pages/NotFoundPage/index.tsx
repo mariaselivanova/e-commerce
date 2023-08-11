735041e3fbbbbe141edb1ad793f9d45449a86511
Dana Karpova <devil13066@gmail.com>
2023-08-11 16:08:10 +0400

    fix: edit NotFounf page Typography and add bg

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,43 +1,45 @@
 import React, { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Typography, Box, Button, Stack } from '@mui/material';
+import { Typography, Box, Button, Stack, Divider } from '@mui/material';
 
 import styles from './NotFoundPage.module.css';
 
 export const NotFoundPage: FC = () => {
   const navigate = useNavigate();
   return (
-    <Stack spacing={'1em'} className={styles.notFoundContainer}>
-      <Typography variant='h3' gutterBottom>
-        404: Lost in Space Gems
-      </Typography>
-      <Typography gutterBottom>
-        Oops! It seems like you&apos;ve ventured into the cosmic void of our website, where our dazzling gems like to play hide-and-seek.
-        <br />
-        While they&apos;re enjoying their little cosmic adventure, we apologize for the temporary detour.
-        <br />
-        Rest assured, our jewelry astronauts are on a mission to retrieve the missing treasures.
-        <br />
-        In the meantime, why not enjoy some stardust humor:
-        <br />
-        <br />
-        <i>Why did the diamond feel left out? It couldn&apos;t find a facet-ating group!</i>
-        <br />
-        <br />
-        If you need assistance finding your way back, reach out to our cosmic customer support team.
-        <br />
-        They&apos;ll navigate you back to the shimmering path.
-        <br />
-        Safe travels through the stars, <br />
-        React Cats Team
-      </Typography>
-      <Stack spacing={'1em'} direction='row'>
-        <Button variant='contained' onClick={() => navigate(-1)}>
-          Back
-        </Button>
-        <Button variant='contained' href='/'>
-          Main
-        </Button>
+    <Stack className={styles.notFoundContainer}>
+      <Stack spacing={'1em'} className={styles.notFoundContainer_text}>
+        <Typography variant='h5' gutterBottom>
+          404: Lost in Space Gems
+        </Typography>
+        <Box>
+          <Typography>
+            Oops! It seems like you&apos;ve ventured into the cosmic void of our website, <br />
+            where our dazzling gems like to play hide-and-seek.
+          </Typography>
+          <Typography>While they&apos;re enjoying their little cosmic adventure, we apologize for the temporary detour.</Typography>
+          <Typography>Rest assured, our jewelry astronauts are on a mission to retrieve the missing treasures.</Typography>
+          <Typography>In the meantime, why not enjoy some stardust humor:</Typography>
+          <Typography className={styles.divider}>
+            <i>Why did the diamond feel left out? It couldn&apos;t find a facet-ating group!</i>
+          </Typography>
+          <Typography>
+            If you need assistance finding your way back, <br />
+            reach out to our cosmic customer support team.
+          </Typography>
+          <Typography>They&apos;ll navigate you back to the shimmering path.</Typography>
+          <Typography>Safe travels through the stars,</Typography>
+          <Typography gutterBottom>React Cats Team</Typography>
+        </Box>
+
+        <Stack spacing={'1em'} direction='row'>
+          <Button variant='contained' onClick={() => navigate(-1)}>
+            Back
+          </Button>
+          <Button variant='contained' href='/'>
+            Main
+          </Button>
+        </Stack>
       </Stack>
     </Stack>
   );
