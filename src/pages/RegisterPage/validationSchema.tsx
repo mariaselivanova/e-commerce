1fa1e8789bfc214218eb94dd4fd635c52d527b94
Pavel <80585863+Svygzhryr@users.noreply.github.com>
2023-08-14 14:41:19 +0300

    other: add variable for 'required' message

--- src/pages/RegisterPage/validationSchema.tsx
+++ src/pages/RegisterPage/validationSchema.tsx
@@ -8,15 +8,16 @@ const nameRules = /^[a-zA-Z]*$/gi;
 const streetRules = /^[a-zA-Z0-9\s]*$/;
 const postalRules = /^\d{6}$/;
 
+const requiredMessage = 'Required field!';
 const minMessage = 'Must be at least 1 character!';
 const nameMessage = 'Must only contain latin characters!';
 const streetMessage = 'Street name can only have latin characters, numbers and whitespaces!';
 
 export const schema = yup.object().shape({
-  email: yup.string().required('Required field!').email(EMAIL_VALIDATION.message).matches(EMAIL_VALIDATION.rules, EMAIL_VALIDATION.message),
+  email: yup.string().required(requiredMessage).email(EMAIL_VALIDATION.message).matches(EMAIL_VALIDATION.rules, EMAIL_VALIDATION.message),
   password: yup
     .string()
-    .required('Required field!')
+    .required(requiredMessage)
     .min(8, 'Password must be at least 8 characters long!')
     .matches(PASSWORD_VALIDATION.rules_uppercase, { message: PASSWORD_VALIDATION.message_uppercase })
     .matches(PASSWORD_VALIDATION.rules_lowercase, { message: PASSWORD_VALIDATION.message_lowercase })
@@ -25,18 +26,18 @@ export const schema = yup.object().shape({
     .string()
     .required('Retype your password!')
     .oneOf([yup.ref('password')], 'Your passwords do not match.'),
-  firstname: yup.string().required('Required field!').min(1, minMessage).matches(nameRules, nameMessage),
-  lastname: yup.string().required('Required field!').min(1, minMessage).matches(nameRules, nameMessage),
+  firstname: yup.string().required(requiredMessage).min(1, minMessage).matches(nameRules, nameMessage),
+  lastname: yup.string().required(requiredMessage).min(1, minMessage).matches(nameRules, nameMessage),
   date: yup
     .date()
     .nullable()
     .typeError('Please type date of a correct format!')
-    .required('Required field!')
+    .required(requiredMessage)
     .max(dateRules, 'You must be at least 13 years old to register!'),
-  billing_street: yup.string().required('Required field!').min(1, minMessage).matches(streetRules, streetMessage),
-  billing_city: yup.string().required('Required field!').min(1, minMessage).matches(nameRules, nameMessage),
-  billing_postal: yup.string().required('Required field!').matches(postalRules, 'Postal code can only contain 6 numbers!'),
-  billing_country: yup.string().required('Required field!'),
+  billing_street: yup.string().required(requiredMessage).min(1, minMessage).matches(streetRules, streetMessage),
+  billing_city: yup.string().required(requiredMessage).min(1, minMessage).matches(nameRules, nameMessage),
+  billing_postal: yup.string().required(requiredMessage).matches(postalRules, 'Postal code can only contain 6 numbers!'),
+  billing_country: yup.string().required(requiredMessage),
 
   shipping_street: yup.string().min(1, minMessage).matches(streetRules, streetMessage),
   shipping_city: yup.string().min(1, minMessage).matches(nameRules, nameMessage),
