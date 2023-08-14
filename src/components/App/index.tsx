7f6a7283cf08c44b879aff777a96c850ff85bd6b
Maria Selivanova <maria.m.selivanova@yandex.ru>
2023-08-14 12:50:40 +0300

    fix: fix linter errors

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { UserContext } from '../../contexts/userContext';
