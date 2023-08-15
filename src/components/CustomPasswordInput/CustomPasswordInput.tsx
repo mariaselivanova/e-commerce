7928f73902c9142d636829e0abe959ed26d1dd51
Svygzhryr <pamixy@gmail.com>
2023-08-15 08:27:11 +0300

    refactor: move reusable schemas to utils/validation.ts

---
+++ src/components/CustomPasswordInput/CustomPasswordInput.tsx
@@ -0,0 +1,3 @@
+import React, { FC } from 'react';
+
+export const CustomPasswordInput: FC = () => <div>CustomPasswordInput</div>;
--- src/pages/LoginPage/validationSchema.tsx
+++ src/pages/LoginPage/validationSchema.tsx
@@ -1,7 +1,13 @@
 import * as yup from 'yup';
-import { emailMessage, passwordMassage, emailRules, passwordRules } from '../../utils/inputRules';
+import { EMAIL_VALIDATION, PASSWORD_VALIDATION } from '../../utils/validation';
 
 export const schemaLogin = yup.object().shape({
-  email: yup.string().required('Required field!').email(emailMessage).matches(emailRules, emailMessage),
-  password: yup.string().required('Required field!').matches(passwordRules, { message: passwordMassage }),
+  email: yup.string().required('Required field!').email(EMAIL_VALIDATION.message).matches(EMAIL_VALIDATION.rules, EMAIL_VALIDATION.message),
+  password: yup
+    .string()
+    .required('Required field!')
+    .min(8, 'Password must be at least 8 characters long!')
+    .matches(PASSWORD_VALIDATION.rules_uppercase, { message: PASSWORD_VALIDATION.message_uppercase })
+    .matches(PASSWORD_VALIDATION.rules_lowercase, { message: PASSWORD_VALIDATION.message_lowercase })
+    .matches(PASSWORD_VALIDATION.rules_digit, { message: PASSWORD_VALIDATION.message_digit }),
 });
