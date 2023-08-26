cf2b5e603eab7df0233ea2ab0e0b013f8d2f14bd
Svygzhryr <pamixy@gmail.com>
2023-08-26 07:40:22 +0300

    other: change registration form

--- src/pages/RegisterPage/index.tsx
+++ src/pages/RegisterPage/index.tsx
@@ -126,8 +126,8 @@ export const RegisterPage: FC = () => {
       ],
 
       // поправить просто billing и просто shipping адреса
-      defaultBillingAddress: data.defaultBilling ? INDEX_ADDRESS_BILLING : 0,
-      defaultShippingAddress: data.defaultShipping ? INDEX_ADDRESS_SHIPPING : 0,
+      defaultBillingAddress: data.defaultBilling ? INDEX_ADDRESS_BILLING : undefined,
+      defaultShippingAddress: data.defaultShipping ? INDEX_ADDRESS_SHIPPING : undefined,
     };
 
     handleUserRegistration(processedData);
