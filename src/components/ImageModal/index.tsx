9ad076fce8dbeea2956e388602ff997898a73c3d
Dana Karpova <devil13066@gmail.com>
2023-09-01 19:08:14 +0400

    feat: add cross icon to modal and add other minor featuers

--- src/components/ImageModal/index.tsx
+++ src/components/ImageModal/index.tsx
@@ -1,7 +1,8 @@
 import React, { FC } from 'react';
-import { Modal, Box } from '@mui/material';
+import { Modal, Box, IconButton } from '@mui/material';
 
 import { ImgSlider } from '../ImgSlider';
+import crossIcon from '../../assets/icons/modal-cross.svg';
 import styles from './ImageModal.module.css';
 
 export interface ImageModalProps {
@@ -14,6 +15,9 @@ export interface ImageModalProps {
 export const ImageModal: FC<ImageModalProps> = ({ open, handleClose, images, imageStep }) => (
   <Modal open={open} onClose={handleClose} className={styles.modal}>
     <Box>
+      <IconButton className={styles.crossIconBtn} onClick={handleClose}>
+        <img src={crossIcon} alt='close icon' className={styles.crossIcon} />
+      </IconButton>
       <ImgSlider images={images} isModal={true} imageStep={imageStep} />
     </Box>
   </Modal>
