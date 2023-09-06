872d13724744b60ee2f46dced8eeae994ac50f39
Svygzhryr <pamixy@gmail.com>
2023-09-06 14:12:17 +0300

    feat: add promo code view on main page

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from 'react';
-import { Typography, Box, Stack, createTheme, Button } from '@mui/material';
+import React, { FC, useContext } from 'react';
+import { Typography, Box, Stack, createTheme, Button, Chip } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 import { useWindowWidth } from '../../hooks/useWindowWidth';
@@ -7,11 +7,14 @@ import { RouteLinks } from '../../utils/types';
 
 import { themeOptionsDark } from '../../theme';
 import styles from './MainPage.module.css';
+import { UserContext } from '../../contexts/userContext';
 
 const themeDark = createTheme(themeOptionsDark);
 
 export const MainPage: FC = () => {
   const { windowWidth, isMobileScreen } = useWindowWidth();
+  const user = useContext(UserContext);
+  const promocodes = ['GOLDEN20', 'DIAMOND50', 'SILVER10', 'PLATINUM35'];
 
   return (
     <div className={styles.mainBg}>
@@ -40,6 +43,18 @@ export const MainPage: FC = () => {
           </Link>
         </Stack>
       </Stack>
+      {user.name ? (
+        <Box color={themeDark.palette.text.primary} className={styles.promocodes}>
+          <Typography variant='h5' component='h5'>
+            Active promocodes:
+          </Typography>
+          <Stack spacing={1} color={themeDark.palette.text.primary} className={styles.promocodesItems}>
+            {promocodes.map((item) => (
+              <Chip color='primary' variant='filled' label={item} key={item} />
+            ))}
+          </Stack>
+        </Box>
+      ) : null}
     </div>
   );
 };
