250e8bd6299ad3563f4b3c817918e9cdca7a4f10
Svygzhryr <pamixy@gmail.com>
2023-09-08 14:36:41 +0300

    feat: add discount code loading from commercetools

--- src/components/DiscountCarousel.tsx
+++ src/components/DiscountCarousel.tsx
@@ -1,46 +1,56 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { autoPlay } from 'react-swipeable-views-utils';
 import SwipeableViews from 'react-swipeable-views';
 
+import { DiscountCode } from '@commercetools/platform-sdk';
 import { Tooltip, Button, Typography, Paper, MobileStepper, Box } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 
+import { useErrorHandling } from '../hooks/useErrorHandling';
+import { getDiscountCodes } from '../sdk/requests';
 import styles from './DiscountCarousel.module.css';
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
-const promocodes = [
-  {
-    code: 'SILVER10',
-    description:
-      'Looking to save some money on your next purchase? Look no further than our 10% Off Everything discount! Looking to save some money on your next purchase? Look no further than our 10% Off Everything discount Looking to save some money on your next purchase? Look no further than our 10% Off Everything discount Looking to save some money on your next purchase? Look no further than our 10% Off Everything discount',
-  },
-  {
-    code: 'GOLDEN20',
-    description: 'Get ready to treat yourself this holiday season with the this exclusive 20% discount, available for a short time!',
-  },
-  {
-    code: 'PLATINUM35',
-    description: 'Mindblowing discount for our beloved customers! Use this 35% off discount code for your next purchase!',
-  },
-  {
-    code: 'DIAMOND50',
-    description: 'Now this is just straight up robbery! 50% off to any product in our shop, dont waste this opportunity!',
-  },
-];
-
 export const DiscountCarousel: FC = () => {
   const theme = useTheme();
   const [activeStep, setActiveStep] = useState(0);
-  const maxSteps = promocodes.length;
 
   const [copySuccess, setCopySuccess] = useState('Click to copy');
+  const [description, setDescription] = useState('');
+  const [discountCodes, setDiscountCodes] = useState<DiscountCode[]>([
+    {
+      id: '',
+      code: '',
+      version: 0,
+      createdAt: '',
+      lastModifiedAt: '',
+      cartDiscounts: [],
+      references: [],
+      groups: [],
+      isActive: false,
+    },
+  ]);
+  const { handleError } = useErrorHandling();
+
+  const maxSteps = discountCodes.length;
+
+  useEffect(() => {
+    getDiscountCodes()
+      .then((data) => {
+        setDiscountCodes(data.body.results.filter((e) => e.isActive));
+      })
+      .catch((err) => {
+        handleError(err);
+        setDescription('An error occured! Cannot get discount codes :(');
+      });
+  }, [handleError]);
 
   const copyToClipBoard = (): void => {
     try {
-      navigator.clipboard.writeText(promocodes[activeStep].code);
+      navigator.clipboard.writeText(discountCodes[activeStep].code);
       setCopySuccess('Copied!');
     } catch (err) {
       setCopySuccess('Failed to copy!');
@@ -66,11 +76,11 @@ export const DiscountCarousel: FC = () => {
   };
 
   return (
-    <Box className={styles.box}>
+    <Box className={styles.box} display={!description ? 'initial' : 'none'}>
       <Tooltip onClose={handleCodeBlur} placement='top' title={copySuccess}>
         <Paper onClick={copyToClipBoard} square elevation={0} className={styles.header}>
           <Typography variant='h6' component='h6' className={styles.code}>
-            {promocodes[activeStep].code}
+            {discountCodes[activeStep].code}
           </Typography>
         </Paper>
       </Tooltip>
@@ -81,8 +91,16 @@ export const DiscountCarousel: FC = () => {
         onChangeIndex={handleStepChange}
         enableMouseEvents
       >
-        {promocodes.map((step, index) => (
-          <div key={step.code}>{Math.abs(activeStep - index) <= 2 ? <Typography className={styles.main}>{step.description}</Typography> : null}</div>
+        {discountCodes.map((step, index) => (
+          <div key={step.code}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Typography className={styles.main}>
+                {step.description?.['en-US'] ||
+                  description ||
+                  'We dont have description for this promocode, but it definitely does something! Why not try it out?'}
+              </Typography>
+            ) : null}
+          </div>
         ))}
       </AutoPlaySwipeableViews>
       <MobileStepper
