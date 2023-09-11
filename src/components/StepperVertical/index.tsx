7ed22afde40b689ea00ddacba31913b65e23f43c
Svygzhryr <pamixy@gmail.com>
2023-09-11 16:00:37 +0300

    feat: add team contribution and responsive

--- src/components/StepperVertical/index.tsx
+++ src/components/StepperVertical/index.tsx
@@ -1,100 +1,9 @@
 import React, { FC, useState } from 'react';
 import { Avatar, Box, Button, Chip, Paper, Stack, Step, StepContent, StepLabel, Stepper, Typography } from '@mui/material';
+import { STEPS } from './steps';
 import styles from './StepperVertical.module.css';
 
 export const StepperVertical: FC = () => {
-  const steps = [
-    {
-      label: 'Sprint #1',
-      tasks: [
-        {
-          task: 'Task board setup',
-          author: 'danuta',
-          description:
-            'The hardest part is the start. We had to start somewhere. So we started from the beginning. We created a task board to help us track our proggress and plan our actions.',
-        },
-        {
-          task: 'Repository setup',
-          author: 'maria',
-          description: 'Then we had to create and configure a Github repo for comfortable development',
-        },
-        {
-          task: 'Set up scripts',
-          author: 'maria',
-          description:
-            'We had to set up necessary scripts and modules for out project... Luckily, we had wonderful team and a great mentor to help us out!',
-        },
-        {
-          task: 'Husky, jest and testing',
-          author: 'pavel',
-          description: "Testing is also important. So we had to make sure the process will go safe and smooth and pushed code won't break anything",
-        },
-        {
-          task: 'Comprehensive readMe',
-          author: 'danuta',
-          description: 'Clear description helps to deliver necessary details to any user that stumble across this project.',
-        },
-        {
-          task: 'Commercetools project and API setup',
-          author: 'maria',
-          description: 'In this sprint we also had to make sure commercetools api is ready for work as well.',
-        },
-      ],
-    },
-    {
-      label: 'Sprint #2',
-      tasks: [
-        {
-          task: '404 page',
-          author: 'danuta',
-          description: '404 error page is necessary when user tries to access unavaliable or missing page',
-        },
-        {
-          task: 'Do this',
-          author: 'maria',
-          description: '',
-        },
-        {
-          task: 'Do that',
-          author: 'danuta',
-          description: '',
-        },
-      ],
-    },
-    {
-      label: 'Sprint #3',
-
-      tasks: [
-        {
-          task: 'Set up scripts',
-          author: 'maria',
-          description: '',
-        },
-        {
-          task: '404 page',
-          author: 'danuta',
-          description: '',
-        },
-      ],
-    },
-    {
-      label: 'Sprint #4',
-
-      tasks: [
-        {
-          task: 'Set up scripts',
-          author: 'maria',
-          description: '',
-        },
-        {
-          task: '404 page',
-          author: 'danuta',
-          description: '',
-        },
-      ],
-    },
-  ];
-
   const [activeStep, setActiveStep] = useState(0);
 
   const handleNext = (): void => {
@@ -108,12 +17,17 @@ export const StepperVertical: FC = () => {
   const handleReset = (): void => {
     setActiveStep(0);
   };
+
+  const handleSetStep = (index: number): void => {
+    setActiveStep(index);
+  };
+
   return (
     <Box className={styles.box}>
       <Stepper variant='outlined' activeStep={activeStep} orientation='vertical'>
-        {steps.map((step, index) => (
+        {STEPS.map((step, index) => (
           <Step key={step.label}>
-            <StepLabel>
+            <StepLabel onClick={(): void => handleSetStep(index)} sx={{ cursor: 'pointer' }}>
               <Typography variant='h5' component='h5'>
                 {step.label}
               </Typography>
@@ -126,12 +40,12 @@ export const StepperVertical: FC = () => {
                   <Typography className={styles.taskDesc}>{e.description}</Typography>
                 </Stack>
               ))}
-              <Box sx={{ mb: 2, mt: 10 }}>
+              <Box className={styles.buttons} sx={{ mb: 2, mt: 10 }}>
                 <div>
-                  <Button variant='contained' onClick={handleNext} sx={{ mt: 1, mr: 1 }}>
-                    {index === steps.length - 1 ? 'Finish' : 'Continue'}
+                  <Button variant='contained' onClick={handleNext} sx={{ mr: 1 }}>
+                    {index === STEPS.length - 1 ? 'Finish!' : 'Continue'}
                   </Button>
-                  <Button disabled={index === 0} onClick={handleBack} sx={{ mt: 1, mr: 1 }}>
+                  <Button variant='outlined' disabled={index === 0} onClick={handleBack}>
                     Back
                   </Button>
                 </div>
@@ -140,10 +54,12 @@ export const StepperVertical: FC = () => {
           </Step>
         ))}
       </Stepper>
-      {activeStep === steps.length && (
+      {activeStep === STEPS.length && (
         <Paper square elevation={0} sx={{ p: 3 }}>
-          <Typography>All steps completed - you&apos;re finished</Typography>
-          <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+          <Typography className={styles.endCredit} variant='h5' component='h5'>
+            Thus the Universe of Sparkle was born! Thank you for coming throughout this journey with us!
+          </Typography>
+          <Button color='primary' variant='contained' onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
             Begin anew!
           </Button>
         </Paper>
--- src/pages/AboutUs/index.tsx
+++ src/pages/AboutUs/index.tsx
@@ -1,95 +1,111 @@
 import React, { FC } from 'react';
-import { Avatar, Box, Chip, Container, Grid, IconButton, Link, Typography } from '@mui/material';
+import { Avatar, Box, Chip, Container, Grid, IconButton, Link, Typography, Stack } from '@mui/material';
 import gitHubIcon from '../../assets/icons/github-mark.svg';
 import rsIcon from '../../assets/icons/rs.svg';
 import { StepperVertical } from '../../components/StepperVertical';
 import styles from './AboutUs.module.css';
+import { useWindowWidth } from '../../hooks/useWindowWidth';
 
-export const AboutUs: FC = () => (
-  <>
-    <Typography variant='h4' component='h4'>
-      Our incomparable team
-    </Typography>
-    <Grid className={styles.grid} container columns={3}>
-      <Grid className={styles.card} item xs={1}>
-        <a href='https://github.com/mariaselivanova'>
-          <Box className={styles.cardImgWrapper}>
-            <section className={styles.cardImgGitWrapper}>
-              <img className={styles.cardImgGit} src={gitHubIcon} />
-            </section>
-            <img className={styles.cardImg} src='https://imgflip.com/s/meme/Cute-Cat.jpg' />
-          </Box>
-        </a>
-        <Box className={styles.infoWrapper}>
-          <Typography variant='h5' component='h5'>
-            Maria Selivanova
-          </Typography>
-          <Chip className={styles.role} color='primary' variant='filled' label='Front-end Developer' />
-          <Typography className={styles.bio}>
-            Im a front-end developer: the creative force behind the digital space. I spend my days dreaming up new and exciting ways to make
-            interactive multimedia experiences come to life. But dont let my creative abilities fool you, Im also a problem-solving machine and will
-            stop at nothing until the project is completed and the client is happy. I have a passion for creating, and the drive to make a difference.
-          </Typography>
-        </Box>
-      </Grid>
-      <Grid className={styles.card} item xs={1}>
-        <a href='https://github.com/Inari13066'>
-          <Box className={styles.cardImgWrapper}>
-            <section className={styles.cardImgGitWrapper}>
-              <img className={styles.cardImgGit} src={gitHubIcon} />
-            </section>
-            <img className={styles.cardImg} src='https://imgflip.com/s/meme/Cute-Cat.jpg' />
-          </Box>
-        </a>
-        <Box className={styles.infoWrapper}>
-          <Typography variant='h5' component='h5'>
-            Danuta Carpava
-          </Typography>
-          <Chip className={styles.role} color='primary' variant='filled' label='Front-end Developer' />
-          <Typography className={styles.bio}>
-            Im a front-end developer: the creative force behind the digital space. I spend my days dreaming up new and exciting ways to make
-            interactive multimedia experiences come to life. But dont let my creative abilities fool you, Im also a problem-solving machine and will
-            stop at nothing until the project is completed and the client is happy. I have a passion for creating, and the drive to make a difference.
-          </Typography>
-        </Box>
-      </Grid>
-      <Grid className={styles.card} item xs={1}>
-        <a href='https://github.com/Svygzhryr'>
-          <Box className={styles.cardImgWrapper}>
-            <section className={styles.cardImgGitWrapper}>
-              <img className={styles.cardImgGit} src={gitHubIcon} />
-            </section>
-            <img className={styles.cardImg} src='https://imgflip.com/s/meme/Cute-Cat.jpg' />
-          </Box>
-        </a>
-        <Box className={styles.infoWrapper}>
-          <Typography variant='h5' component='h5'>
-            Pavel Mihailov
-          </Typography>
-          <Chip className={styles.role} color='primary' variant='filled' label='Front-end Developer' />
-          <Typography className={styles.bio}>
-            Born in Mogilev, Belarus, graduated a Belarusian-Russian University with a degree in biotechnical systems and technologies. Strives to
-            learn Front-end because this is what he always wanted to do <br /> (except of becoming a president, flying to the moon and stopping global
-            warming of course).
-          </Typography>
-        </Box>
-      </Grid>
-    </Grid>
-    <Box className={styles.contribution}>
-      <Typography className={styles.header} variant='h4' component='h4'>
-        Contributions to the project
+export const AboutUs: FC = () => {
+  const windowDimensions = useWindowWidth();
+
+  return (
+    <>
+      <Typography className={styles.team} variant='h4' component='h4'>
+        Our incomparable team
       </Typography>
-      <Container className={styles.avatarContainer}>
-        <Avatar className={styles.maria}>M</Avatar>
-        <Avatar className={styles.danuta}>D</Avatar>
-        <Avatar className={styles.pavel}>P</Avatar>
-      </Container>
-      <StepperVertical />
-    </Box>
-    <Link href='https://rs.school/index.html'>
-      <IconButton>
-        <img className={styles.rsIcon} src={rsIcon} />
-      </IconButton>
-    </Link>
-  </>
-);
+      <Grid className={styles.grid} container columns={windowDimensions.windowWidth <= 830 ? 1 : 3}>
+        <Grid className={styles.card} item xs={1}>
+          <a href='https://github.com/mariaselivanova'>
+            <Box className={styles.cardImgWrapper}>
+              <section className={styles.cardImgGitWrapper}>
+                <img className={styles.cardImgGit} src={gitHubIcon} />
+              </section>
+              <img className={styles.cardImg} src='https://imgflip.com/s/meme/Cute-Cat.jpg' />
+            </Box>
+          </a>
+          <Box className={styles.infoWrapper}>
+            <Typography variant='h5' component='h5'>
+              Maria Selivanova
+            </Typography>
+            <Chip className={styles.role} color='primary' variant='filled' label='Front-end Developer' />
+            <Typography className={styles.bio}>
+              Im a front-end developer: the creative force behind the digital space. I spend my days dreaming up new and exciting ways to make
+              interactive multimedia experiences come to life. But dont let my creative abilities fool you, Im also a problem-solving machine and will
+              stop at nothing until the project is completed and the client is happy. I have a passion for creating, and the drive to make a
+              difference.
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid className={styles.card} item xs={1}>
+          <a href='https://github.com/Inari13066'>
+            <Box className={styles.cardImgWrapper}>
+              <section className={styles.cardImgGitWrapper}>
+                <img className={styles.cardImgGit} src={gitHubIcon} />
+              </section>
+              <img className={styles.cardImg} src='https://imgflip.com/s/meme/Cute-Cat.jpg' />
+            </Box>
+          </a>
+          <Box className={styles.infoWrapper}>
+            <Typography variant='h5' component='h5'>
+              Danuta Carpava
+            </Typography>
+            <Chip className={styles.role} color='primary' variant='filled' label='Front-end Developer' />
+            <Typography className={styles.bio}>
+              Im a front-end developer: the creative force behind the digital space. I spend my days dreaming up new and exciting ways to make
+              interactive multimedia experiences come to life. But dont let my creative abilities fool you, Im also a problem-solving machine and will
+              stop at nothing until the project is completed and the client is happy. I have a passion for creating, and the drive to make a
+              difference.
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid className={styles.card} item xs={1}>
+          <a href='https://github.com/Svygzhryr'>
+            <Box className={styles.cardImgWrapper}>
+              <section className={styles.cardImgGitWrapper}>
+                <img className={styles.cardImgGit} src={gitHubIcon} />
+              </section>
+              <img className={styles.cardImg} src='https://imgflip.com/s/meme/Cute-Cat.jpg' />
+            </Box>
+          </a>
+          <Box className={styles.infoWrapper}>
+            <Typography variant='h5' component='h5'>
+              Pavel Mihailov
+            </Typography>
+            <Chip className={styles.role} color='primary' variant='filled' label='Front-end Developer' />
+            <Typography className={styles.bio}>
+              Born in Mogilev, Belarus, graduated a Belarusian-Russian University with a degree in biotechnical systems and technologies. Strives to
+              learn Front-end because this is what he always wanted to do. May and will be lazy sometimes but for sure will try his best. (on the last
+              day before the deadline)
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+      <Box className={styles.contribution}>
+        <Typography className={styles.header} variant='h4' component='h4'>
+          Contributions to the project
+        </Typography>
+        <Container className={styles.avatarContainer}>
+          <Stack>
+            <Avatar className={styles.maria}>M</Avatar>
+            <Typography>Maria</Typography>
+          </Stack>
+          <Stack>
+            <Avatar className={styles.danuta}>D</Avatar>
+            <Typography>Danuta</Typography>
+          </Stack>
+          <Stack>
+            <Avatar className={styles.pavel}>P</Avatar>
+            <Typography>Pavel</Typography>
+          </Stack>
+        </Container>
+        <StepperVertical />
+      </Box>
+      <Link href='https://rs.school/index.html'>
+        <IconButton className={styles.rsIconWrapper}>
+          <img className={styles.rsIcon} src={rsIcon} />
+        </IconButton>
+      </Link>
+    </>
+  );
+};
