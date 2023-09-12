da890a3a4abdd1f983672218fa4fb0ad99db8fe3
Svygzhryr <pamixy@gmail.com>
2023-09-12 21:08:57 +0300

    refactor: move creator info to utils

--- src/pages/AboutUs/index.tsx
+++ src/pages/AboutUs/index.tsx
@@ -6,32 +6,12 @@ import { StepperVertical } from '../../components/StepperVertical';
 
 import rsIcon from '../../assets/icons/rs.svg';
 
-import styles from './AboutUs.module.css';
 import { AboutCard } from '../../components/AboutCard';
+import { creatorsInfo } from '../../utils/creatorsInfo';
+import styles from './AboutUs.module.css';
 
 export const AboutUs: FC = () => {
   const windowDimensions = useWindowWidth();
-
-  const creatorsInfo = [
-    {
-      name: 'Maria Selivanova',
-      photo: 'https://imgflip.com/s/meme/Cute-Cat.jpg',
-      github: 'https://github.com/mariaselivanova',
-      bio: 'Im a front-end developer: the creative force behind the digital space. I spend my days dreaming up new and exciting ways to make interactive multimedia experiences come to life. But dont let my creative abilities fool you, Im also a problem-solving machine and will stop at nothing until the project is completed and the client is happy. I have a passion for creating, and the drive to make a difference.',
-    },
-    {
-      name: 'Danuta Carpava',
-      photo: 'https://imgflip.com/s/meme/Cute-Cat.jpg',
-      github: 'https://github.com/Inari13066',
-      bio: 'Im a front-end developer: the creative force behind the digital space. I spend my days dreaming up new and exciting ways to make interactive multimedia experiences come to life. But dont let my creative abilities fool you, Im also a problem-solving machine and will stop at nothing until the project is completed and the client is happy. I have a passion for creating, and the drive to make a difference.',
-    },
-    {
-      name: 'Pavel Mihailov',
-      photo: 'https://i.imgur.com/eo3IDdK.jpg',
-      github: 'https://github.com/Svygzhryr',
-      bio: 'Born in Mogilev, Belarus, graduated a Belarusian-Russian University with a degree in biotechnical systems and technologies. Strives to learn Front-end because this is what he always wanted to do. May and will be lazy sometimes but for sure will try his best. (on the last day before the deadline)',
-    },
-  ];
 
   return (
     <>
