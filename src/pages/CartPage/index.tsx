0f7ffe87149b2270a3665f5bde4505c8a6f5510d
Svygzhryr <pamixy@gmail.com>
2023-09-13 14:35:44 +0300

    feat: add price, name, photo to items on cart page

--- src/pages/CartPage/index.tsx
+++ src/pages/CartPage/index.tsx
@@ -1,11 +1,13 @@
 import React, { FC, useEffect, useState, useContext } from 'react';
-import { Button, Stack, Typography } from '@mui/material';
+import { Button, Typography, TableContainer, TableCell, TableHead, TableRow, TableBody, Table } from '@mui/material';
 import { Cart } from '@commercetools/platform-sdk';
 
 import { useErrorHandling } from '../../hooks/useErrorHandling';
 import { UserMessage } from '../../components/UserMessage';
 import { getCartById, deleteCart } from '../../sdk/requests';
 import { UserContext } from '../../contexts/userContext';
+
+import styles from './CartPage.module.css';
 
 export const CartPage: FC = () => {
   const { errorState, closeError, handleError } = useErrorHandling();
@@ -35,6 +37,26 @@ export const CartPage: FC = () => {
     }
   };
 
+  const calculateTotalPrice = (): string => {
+    let totalPrice = 0;
+    myCart?.lineItems.forEach((item) => {
+      totalPrice += item.totalPrice.centAmount / 100;
+    });
+    return `${totalPrice}$`;
+  };
+
+  const normalizeName = (name: string): string => {
+    if (!name) {
+      return 'N/A';
+    }
+
+    return name
+      .toLowerCase()
+      .split(' ')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  };
+
   return (
     <>
       {errorState.isError && (
@@ -43,21 +65,46 @@ export const CartPage: FC = () => {
         </UserMessage>
       )}
       <Typography gutterBottom variant='h4'>
-        Cart
+        Your Cart
       </Typography>
-      {myCart ? (
-        <Stack>
-          <Typography gutterBottom>CART ID: {user.cart}</Typography>
-          {myCart.lineItems.map((item) => (
-            <Stack direction='row' justifyContent='space-between' key={item.productKey}>
-              <Typography>{item.productKey}</Typography>
-              <Typography>quantity:{item.quantity}</Typography>
-            </Stack>
-          ))}
-          <Button onClick={handleRemoveCart}>emptyCart</Button>
-        </Stack>
+      {myCart?.lineItems.length ? (
+        <TableContainer className={styles.cart}>
+          <Table>
+            <TableHead className={styles.head}>
+              <TableRow>
+                <TableCell align='center'>Image</TableCell>
+                <TableCell align='center'>Name</TableCell>
+                <TableCell align='center'>Quantity</TableCell>
+                <TableCell align='center'>Price</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {myCart.lineItems.map((item) => (
+                <TableRow className={styles.item} key={item.productKey}>
+                  <TableCell align='center'>
+                    <img className={styles.image} src={item.variant.images ? item.variant.images[0].url : ''} />
+                  </TableCell>
+                  <TableCell align='center'>{normalizeName(item.name['en-US'])}</TableCell>
+                  <TableCell align='center'>{item.quantity}</TableCell>
+                  <TableCell align='center'>{item.totalPrice.centAmount / 100}$</TableCell>
+                </TableRow>
+              ))}
+              <TableRow className={styles.item}>
+                <TableCell />
+                <TableCell />
+                <TableCell align='center'>Total:</TableCell>
+                <TableCell align='center'>{calculateTotalPrice()}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+          <Button variant='contained' className={styles.clear} onClick={handleRemoveCart}>
+            Clear
+          </Button>
+        </TableContainer>
       ) : (
-        'No products'
+        <Typography variant='h5' component='h5'>
+          No products!
+        </Typography>
       )}
     </>
   );
